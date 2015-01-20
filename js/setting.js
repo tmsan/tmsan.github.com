b926d51a4b6940b192ec93a0c3dc5ededb508388
tmsan <e2111046@ed.hyogo-dai.ac.jp>
2015-01-20 12:31:44 +0900

    a
    a

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'takasa_2013';
+OpenSpending.identifier = 'takasago_2013';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
